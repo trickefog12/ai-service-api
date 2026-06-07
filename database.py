3cc6725e83d9e4112fa455f40882ea902446ee77
trickefog12 <cvatistas@icloud.com>
2026-06-07 23:27:18 +0300

    Add pytest unit tests with Stripe mocking

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
